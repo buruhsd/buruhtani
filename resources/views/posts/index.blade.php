3df0c73132b7c96d28808d82532ec6e1e0669b87
buruhsd <buruhsd@outlook.co.id>
2022-07-03 11:10:52 +0700

    update view on index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
     <div class="container mx-auto p-5">
-        <h1 class="text-4xl mt-32 text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+        <!-- <h1 class="text-4xl mt-32 text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
             Welcome to The Blog
-        </h1>
+        </h1> -->
 
         <div class="mt-10 max-w-xl mx-auto">
             @foreach($posts as $post)
